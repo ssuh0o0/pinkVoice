a707f9a3761ceabdbe3a62115fa5c5ec344c6631
ssuh0o0 <ssuh0o0@daum.net>
2021-02-04 02:36:36 +0900

    can load MainActivity

--- android/app/src/main/java/com/example/myapplication3/MainActivity.java
+++ android/app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
     }
 
     @Override
