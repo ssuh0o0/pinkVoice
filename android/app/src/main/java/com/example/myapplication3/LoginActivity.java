d36dfb4ce11e9a0bbc42d1f2c03970b2e19d09c2
Eun Young <eyeyl0811@naver.com>
2021-02-10 02:24:37 +0900

    키보드 터치 제어

--- android/app/src/main/java/com/example/myapplication3/LoginActivity.java
+++ android/app/src/main/java/com/example/myapplication3/LoginActivity.java
@@ -4,14 +4,40 @@ package com.example.myapplication3;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class LoginActivity extends Activity {
+
+    View loginBack;
+
+    private DatabaseReference mDatabase;
+
+    private static final String LOGINTAB = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final InputMethodManager keyboardManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+
+        loginBack = findViewById(R.id.loginBack);
+
+        // firebase 정의
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // 배경 터치시 키보드 내리기
+        loginBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                keyboardManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            }
+        });
 
         Button registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(v -> {
