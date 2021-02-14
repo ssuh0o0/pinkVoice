03f89af7a2fd00ce4a1745fa6dfecba5196ca0ea
zio <jiho8345@naver.com>
2021-02-14 16:40:12 +0900

    스캔캡처폼수정

--- pinkVoice-main/android/app/src/main/java/com/example/myapplication3/CaptureForm.java
+++ pinkVoice-main/android/app/src/main/java/com/example/myapplication3/CaptureForm.java
@@ -1,6 +1,42 @@
 package com.example.myapplication3;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class CaptureForm extends CaptureActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+
+        //상단 텍스트뷰
+        TextView textView = new TextView(this);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT));
+        textView.setTextColor(Color.parseColor("#3D0925"));
+        textView.setText("\n착석을 위해서는 좌석 상단의\nQR 코드를 스캔하여 주세요.");
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+        GradientDrawable gd = new GradientDrawable();
+        //gd.setColor(Color.parseColor("#F380A1"));
+        gd.setCornerRadius(5);
+        gd.setStroke(3, Color.parseColor("#F380A1"));
+        textView.setBackground(gd);
+
+        //상단 텍스트뷰 추가
+        this.addContentView(textView, params);
+    }
 }
